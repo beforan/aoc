8ba4c03419217111119704685a9f5821eaaa8f02
Jon Couldridge <jcouldridge@outlook.com>
2020-12-15 10:29:23 +0000

    main can launch day/puzzle solutions now

--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,22 @@
-import { Puzzle1 } from "./day1/day1.ts";
+/** @module main Consider this a launcher for each completed day and puzzle */
+import * as Day1 from "./day1/day1.ts";
 
-Puzzle1();
+// Add Days and Puzzles here
+const actions: ActionLookup = {
+  "1,1": Day1.Puzzle1,
+};
+
+type ActionLookup = {
+  [key: string]: () => Promise<void>;
+};
+
+// Launcher Runtime Plumbing
+const [day, puzzle] = Deno.args;
+const key = `${day},${puzzle}`;
+
+const notFound = () =>
+  console.log(
+    `Couldn't find the combination of Day ${day} and Puzzle ${puzzle}.`
+  );
+
+actions[key]?.() ?? notFound();
